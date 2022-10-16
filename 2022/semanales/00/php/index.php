fe5d358269dc7dbc33b30ede1228cf1ffb8f0d7d
jesusAG <33084455+jesusAG28@users.noreply.github.com>
2022-10-16 13:22:16 +0200

    Update index.php

--- 2022/semanales/00/php/index.php
+++ 2022/semanales/00/php/index.php
@@ -3,6 +3,7 @@
 for ($i = 1; $i <= 100; $i++) {
     $m3 = ($i % 3 == 0) ? true : false;
     $m5 = ($i % 5 == 0) ? true : false;
+    
     if (!$m3 && !$m5)
         print_r($i . PHP_EOL);
     elseif ($m3 && $m5)
